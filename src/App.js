70b385c4211e564f562d7634e83778ab1990a6d7
John Moran <johnnymoran44@hotmail.com>
2019-06-19 17:20:03 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      artistName: []
+      artistName: [],
+      image: []
     };
   }
   componentDidMount() {
@@ -21,8 +22,9 @@ class App extends Component {
       .then(response => response.json())
       .then(data =>
         this.setState({
-          artistName: data.artists.items[0].name,
-          log: console.log(data)
+          artistName: data.artists.items[0],
+          log: console.log(data),
+          image: data.artists.items[0].images[0].url
         })
       );
   }
@@ -31,7 +33,10 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Johns latest songs</h1>
-        <p>{this.state.artistName}</p>
+        <p>{this.state.artistName.name}</p>
+        <p>{this.state.artistName.id}</p>
+        <p>{this.state.image}</p>
+        <img src={this.state.image} alt="group image" width="200" />
       </div>
     );
   }
