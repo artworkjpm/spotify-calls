fd56e80dfc74af2e11d088c338700eb9febdf782
John Moran <johnnymoran44@hotmail.com>
2019-06-26 12:58:38 +0200

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,19 +5,19 @@ var bands = require("../festivals/bands.json");
 const Home = props => {
   return (
     <div className="Home">
-      <>
-        <h1>Hi {props.username}!</h1>
-        <h2>Most popular Artist's songs on Spotify</h2>
-        <SearchBands
-          onSubmitValue={props.onSubmitValue}
-          value={props.value}
-          onChangeValue={props.onChangeValue}
-        />
-        <p>
-          {props.artistName.name}, artist id: {props.artistName.id}
-        </p>
-        <img src={props.artistImage} alt="group" width="200" />
+      <h1>Hi {props.username}!</h1>
+      <h2>Most popular Artist's songs on Spotify</h2>
+      <SearchBands
+        onSubmitValue={props.onSubmitValue}
+        value={props.value}
+        onChangeValue={props.onChangeValue}
+      />
+      <p>
+        {props.artistName.name}, artist id: {props.artistName.id}
+      </p>
 
+      <div className="player-wrap">
+        <img src={props.artistImage} alt="group" width="200" height="200" />
         <iframe
           title="spotify player"
           src={`https://open.spotify.com/embed/track/${props.popularSong}`}
@@ -28,36 +28,42 @@ const Home = props => {
           allowtransparency="true"
           allow="encrypted-media"
         />
-
-        <div className="main-wrap">
-          <div className="center-div">
-            <h2>
-              {bands.festival}: {bands.name}
-            </h2>
-            <ul className="center-list">
-              {bands.events.map((group, i) => (
-                <li key={i}>
-                  <button value={group.name} onClick={props.handleClickGroup}>
-                    {group.name}
-                  </button>
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div className="center-div">
-            <h2>{props.artistName.name}'s most popular songs:</h2>
-            <ol className="center-list">
-              {props.popularSongsArray.map((song, i) => (
-                <li key={i}>
-                  <button value={song.id} onClick={props.handleClickSong}>
-                    {song.name}
-                  </button>
-                </li>
-              ))}
-            </ol>
-          </div>
+        <div className="center-div">
+          <h2>{props.artistName.name}'s most popular songs:</h2>
+          <ol className="center-list">
+            {props.popularSongsArray.map((song, i) => (
+              <li key={i}>
+                <button value={song.id} onClick={props.handleClickSong}>
+                  {song.name}
+                </button>
+              </li>
+            ))}
+          </ol>
         </div>
-      </>
+      </div>
+
+      <div className="main-wrap">
+        <div className="center-div">
+          <h2>
+            {bands.festival}: {bands.name}
+          </h2>
+          <table className="center-list">
+            <tbody>
+              {bands.events.map((group, i) => (
+                <tr key={i}>
+                  <td>{group.start}</td>
+                  <td>
+                    {" "}
+                    <button value={group.name} onClick={props.handleClickGroup}>
+                      {group.name}
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 };
