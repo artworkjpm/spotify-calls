4786ee7dffb29d317bbcd89762f0a7d552e7e49b
John <johnnymoran44@hotmail.com>
2019-09-04 10:45:26 +0200

    AllFestivals added

--- src/components/FestivalTable.js
+++ src/components/FestivalTable.js
@@ -8,6 +8,11 @@ import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
 import Chip from "@material-ui/core/Chip";
 import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import AllFestivals from "../festivals/AllFestivals";
+
+const G = AllFestivals[0].name;
+
+console.log("G:", G);
 
 const FestivalTable = props => {
   const [currentIndex, setIndex] = React.useState(0);
---
+++ src/festivals/AllFestivals.js
@@ -0,0 +1,15 @@
+import Glasto2019 from "./Glastonbury2019.json";
+import Reading2019 from "./read2019.json";
+
+const AllFestivals = [
+  {
+    SearchName: "Glastonbury 2019",
+    name: Glasto2019
+  },
+  {
+    SearchName: "Reading 2019",
+    name: Reading2019
+  }
+];
+
+export default AllFestivals;
