e53b5355f8e3bf423fa31b77c7fe838cebd17781
John <johnnymoran44@hotmail.com>
2019-09-06 16:39:20 +0200

    primavera sound

--- src/festivals/AllFestivals.js
+++ src/festivals/AllFestivals.js
@@ -1,5 +1,6 @@
 import Glasto2019 from "./Glastonbury2019.json";
 import Reading2019 from "./read2019.json";
+import primaverasoundbarcelona2019 from "./primaverasoundbarcelona2019.json";
 
 const AllFestivals = [
   {
@@ -17,6 +18,14 @@ const AllFestivals = [
     end: "2019-06-30 12:45",
     Country: "UK",
     attendance: 90000
+  },
+  {
+    SearchName: "Primavera Sound Barcelona 2019",
+    name: primaverasoundbarcelona2019,
+    start: "2019-05-27 20:00",
+    end: "2019-06-03 05:00",
+    Country: "Spain",
+    attendance: 64500
   }
 ];
 
